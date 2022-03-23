f8f0b6f208789433c01ac7914a6231bde17870e1
DavidGfeller <davidgfeller@adms-macbook-pro-2.home>
2022-03-23 08:20:49 +0100

    Version 2.2

--- lib/PRIME.cc
+++ lib/PRIME.cc
@@ -274,7 +274,7 @@ int main(int argc, char ** argv){
     fprintf (pFile, "# PRIME is freely available for academic users.\n");
     fprintf (pFile, "# Private companies should contact eauffarth@licr.org at the Ludwig Institute for Cancer Research Ltd for commercial licenses.\n");
     fprintf (pFile, "# To cite PRIME2.0, please refer to:\n");
-    fprintf (pFile, "# Gfeller et al. Improved predictions of immunogenicity reveal SARS-COV2 CD8 T-cell epitopes, BioRxiv (2022).\n");
+    fprintf (pFile, "# Gfeller et al. Improved predictions of immunogenicity reveal SARS-Cov-2 CD8 T-cell epitopes, BioRxiv (2022).\n");
     fprintf (pFile, "####################\n");
 
     fprintf (pFile, "Peptide\t\%%Rank_bestAllele\tScore_bestAllele\t\%%RankBinding_bestAllele\tBestAllele");
